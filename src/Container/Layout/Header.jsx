756b5c611d36a6e22186035422f38a48c8109538
Luan Cai <cla1136@163.com>
2019-05-03 16:36:30 +0800

    react fenzhi

---
+++ src/Container/Layout/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import styles from './index.css'
+import  './index.less'
+
+class Header extends React.PureComponent{
+    constructor(){
+        super()
+    }
+    render(){
+        return <header className = "header">
+            head
+        </header>
+    }
+}
+export default Header;
